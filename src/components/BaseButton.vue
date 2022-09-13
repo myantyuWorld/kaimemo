301351974c2f4273b6463018a9fff69d44a96c6d
yuichi.oba <yuichi.oba@sorich.jp>
2022-09-13 23:34:14 +0900

    BaseButtonコンポーネント作成

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,13 @@
+<template>
+    <button class="btn" :class="props.color" @click="onClick"><slot/></button>
+</template>
+
+<script setup>
+    const props = defineProps({color: String})
+    const emit = defineEmits(['on-click'])
+    const onClick = () => {
+        emit('on-click')
+    }
+    
+    const aaa = () => {}
+</script>
--- src/components/GoodsRegistForm.vue
+++ src/components/GoodsRegistForm.vue
@@ -2,7 +2,17 @@
     <div class="card shadow">
         <div class="card-body">
             <div>
-                <h3>ITEM REGIST.<button class="btn btn-secondary" @click="initRegist">初期化</button></h3>
+                <h3>ITEM REGIST.
+                    <BaseButon color="btn-secondary" @on-click="initRegist">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                            class="bi bi-arrow-counterclockwise" viewBox="0 0 16 16">
+                            <path fill-rule="evenodd"
+                                d="M8 3a5 5 0 1 1-4.546 2.914.5.5 0 0 0-.908-.417A6 6 0 1 0 8 2v1z" />
+                            <path
+                                d="M8 4.466V.534a.25.25 0 0 0-.41-.192L5.23 2.308a.25.25 0 0 0 0 .384l2.36 1.966A.25.25 0 0 0 8 4.466z" />
+                        </svg>
+                    </BaseButon>
+                </h3>
             </div>
             <div class="row">
                 <div class="col-auto">
@@ -25,8 +35,23 @@
                     </div>
                 </div>
                 <div class="col-2">
-                    <button type="submit" class="btn btn-info" @click="editGoods" v-show="isEditRef">編</button>
-                    <button type="submit" class="btn btn-primary" @click="registGoods" v-show="!isEditRef">追</button>
+                    <BaseButon color="btn-info" @on-click="editGoods" v-show="isEditRef">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                            fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+                            <path
+                                d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
+                            <path fill-rule="evenodd"
+                                d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z" />
+                        </svg>
+                    </BaseButon>
+                    <BaseButon color="btn-primary" @on-click="registGoods" v-show="!isEditRef">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                            fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16">
+                            <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                            <path
+                                d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
+                        </svg>
+                    </BaseButon>
                 </div>
             </div>
         </div>
@@ -36,21 +61,30 @@
             <div class="" v-for="item in memoListRef" :key="item.id">
                 <div class="row" :class="{fin: item.checked}">
                     <div class="col">
-                        <input 
-                        type="checkbox" 
-                        class="check" 
-                        @click="changeCheck(item.id)" 
-                        :checked="item.checked"
-                        />
+                        <input type="checkbox" class="check" @click="changeCheck(item.id)" :checked="item.checked" />
                     </div>
                     <div class="col-6">
                         <label :class="{fin: item.checked}">{{ item.memo}}</label>
                     </div>
                     <div class="col">
-                        <button class="btn btn-info" @click="showMemo(item.id)">編</button>
+                        <BaseButon color="btn-info" @on-click="showMemo(item.id)">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                                fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+                                <path
+                                    d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
+                                <path fill-rule="evenodd"
+                                    d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z" />
+                            </svg>
+                        </BaseButon>
                     </div>
                     <div class="col">
-                        <button class="btn btn-danger" @click="deleteGoods(item.id)">削</button>
+                        <BaseButon color="btn-danger" @on-click="deleteGoods(item.id)">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                                fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
+                                <path fill-rule="evenodd"
+                                    d="M6.5 1a.5.5 0 0 0-.5.5v1h4v-1a.5.5 0 0 0-.5-.5h-3ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1H3.042l.846 10.58a1 1 0 0 0 .997.92h6.23a1 1 0 0 0 .997-.92l.846-10.58Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z" />
+                            </svg>
+                        </BaseButon>
                     </div>
                 </div>
             </div>
@@ -61,19 +95,29 @@
 <script setup>
 import { ref } from 'vue';
 import { useMemoList } from '../composables/useMemoList'
+import BaseButon from './BaseButton.vue'
 
 const memoRef = ref('')
 const foodCheckRef = ref(true)
 const nessesaryCheckRef = ref(true)
 const isEditRef = ref(false)
-const { 
-    memoListRef, 
-    add, 
-    show, 
-    edit, 
+const {
+    memoListRef,
+    add,
+    show,
+    edit,
     del,
     check,
 } = useMemoList()
+
+// -----
+const test = () => {
+    console.log("call test!")
+}
+
+
+
+// -----
 
 const initRegist = () => {
     isEditRef.value = false
@@ -106,4 +150,6 @@ const changeCheck = (id) => {
 }
 </script>
 
-<style></style>
+<style>
+
+</style>
