fb42eea13260a52f72a7d3729d27889213ff48ab
yuichi.oba <yuichi.oba@sorich.jp>
2022-09-20 10:11:50 +0900

    ユーザーID付きでない遷移時、ログオン画面にリダイレクトするよう修正

--- src/route/RouteGoodsList.vue
+++ src/route/RouteGoodsList.vue
@@ -1,26 +1,20 @@
 <template>
-  <div class="wrapper">
-    <router-link to="/second">一週間分の買い物金額入力</router-link>
-    <div class="">
-      <h1 class="title">KAIMEMO!</h1>
-      <GoodsRegistForm 
-      v-model:memo-ref="memoComputed"
-      :is-edit-ref="isEditRef"
-      @init-click="initRegist" 
-      @edit-goods="editGoods" 
-      @regist-goods="registGoods" />
+    <div class="wrapper">
+        <router-link to="/second">一週間分の買い物金額入力</router-link>
+        <div class="">
+            <h1 class="title">KAIMEMO!</h1>
+            <GoodsRegistForm v-model:memo-ref="memoComputed" :is-edit-ref="isEditRef" @init-click="initRegist"
+                @edit-goods="editGoods" @regist-goods="registGoods" />
 
-      <GoodsListVue 
-      :memo-list-ref="memoListRef" 
-      @show-memo="showMemo"
-      @change-check="changeCheck"
-      @delete-goods="deleteGoods"/>
+            <GoodsListVue :memo-list-ref="memoListRef" @show-memo="showMemo" @change-check="changeCheck"
+                @delete-goods="deleteGoods" />
+        </div>
     </div>
-  </div>
 </template>
 
 <script setup>
 import { ref, computed } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
 import GoodsRegistForm from "../components/GoodsRegistForm.vue";
 import GoodsListVue from "../components/goods-list/GoodsList.vue";
 import { useMemoList } from '../composables/useMemoList'
@@ -30,58 +24,64 @@ import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 const memoRef = ref('')
 const isEditRef = ref(false)
+const route = useRoute()
+const router = useRouter()
+
+if (!route.query.user_id) {
+    router.push({name: 'LogOn'})
+}
 
 // 
 const memoComputed = computed({
-  get: () => memoRef.value,
-  set: (value) => (memoRef.value = value),
+    get: () => memoRef.value,
+    set: (value) => (memoRef.value = value),
 })
 // ---- メモサービス
 
 const {
-  memoListRef,
-  add,
-  show,
-  edit,
-  del,
-  check,
+    memoListRef,
+    add,
+    show,
+    edit,
+    del,
+    check,
 } = useMemoList()
 
 const initRegist = () => {
-  console.log("call init regist!")
-  isEditRef.value = false
-  memoRef.value = ''
+    console.log("call init regist!")
+    isEditRef.value = false
+    memoRef.value = ''
 }
 
 const registGoods = (category) => {
-  console.log("call regist goods")
-  console.log(memoRef.value)
+    console.log("call regist goods")
+    console.log(memoRef.value)
 
-  add(memoRef.value, category)
-  memoRef.value = ''
+    add(memoRef.value, category)
+    memoRef.value = ''
 }
 
 const showMemo = (id) => {
-  console.log("call show memo")
-  console.log(id)
+    console.log("call show memo")
+    console.log(id)
 
-  memoRef.value = show(id)
-  isEditRef.value = true
+    memoRef.value = show(id)
+    isEditRef.value = true
 }
 
 const editGoods = () => {
-  edit(memoRef.value)
+    edit(memoRef.value)
 
-  isEditRef.value = false
-  memoRef.value = ''
+    isEditRef.value = false
+    memoRef.value = ''
 }
 
 const deleteGoods = (id) => {
-  del(id)
+    del(id)
 }
 
 const changeCheck = (id) => {
-  check(id)
+    check(id)
 }
 </script>
 
