e613eb556d3c12f2ba5a84565a981164f51c7228
yuichi.oba <yuichi.oba@sorich.jp>
2022-09-30 00:16:56 +0900

    design modify

--- src/components/btn/BaseButton.vue
+++ src/components/btn/BaseButton.vue
@@ -1,5 +1,5 @@
 <template>
-    <button class="btn btn-xs" :class="props.color" @click="onClick"><slot/></button>
+    <button class="btn btn-xs box-shadow--16dp" :class="props.color" @click="onClick"><slot/></button>
 </template>
 
 <script setup>
@@ -9,3 +9,12 @@
         emit('on-click')
     }
 </script>
+
+<style>
+    .box-shadow--8dp {
+    box-shadow: 0 8px 10px 1px rgba(0, 0, 0, .14), 0 3px 14px 2px rgba(0, 0, 0, .12), 0 5px 5px -3px rgba(0, 0, 0, .2)
+}
+.box-shadow--16dp {
+    box-shadow: 0 16px 24px 2px rgba(0, 0, 0, .14), 0 6px 30px 5px rgba(0, 0, 0, .12), 0 8px 10px -5px rgba(0, 0, 0, .2)
+}
+</style>
